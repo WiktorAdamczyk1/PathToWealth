a4b9dc661198808eedc1a6f4669ac600f19d5365
Wiktor Adamczyk <67286353+WiktorAdamczyk1@users.noreply.github.com>
2024-01-08 23:33:09 +0100

    Data layer with Entity Framework and data models

---
+++ PathToWealthAPI/PathToWealthAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static PathToWealthAPI.Data.Models;
+
+namespace PathToWealthAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<JwtToken> JwtTokens { get; set; }
+        public DbSet<UserFinancialData> UserFinancialData { get; set; }
+
+    }
+}
---
+++ PathToWealthAPI/PathToWealthAPI/Data/Models.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PathToWealthAPI.Data
+{
+    public class Models
+    {
+
+        public class User
+        {
+            public int UserId { get; set; }
+            [Required]
+            public string Username { get; set; }
+            [Required]
+            public string PasswordHash { get; set; }
+            [Required]
+            public string Email { get; set; }
+        }
+
+        public class JwtToken
+        {
+            public int TokenId { get; set; }
+            public int UserId { get; set; }
+            [Required]
+            public string Token { get; set; }
+            [Required]
+            public DateTime ExpiryDate { get; set; }
+        }
+
+        public class UserFinancialData
+        {
+            public int DataId { get; set; }
+            public int UserId { get; set; }
+            public int Age { get; set; }
+            public decimal InitialInvestment { get; set; }
+            public decimal AnnualIncome { get; set; }
+            public decimal InvestmentPercentage { get; set; }
+            public int RetirementAge { get; set; }
+            public decimal AnnualRetirementIncome { get; set; }
+            public decimal FundBondRatio { get; set; }
+            public string PreferredFundsBonds { get; set; }
+            public int HistoricalInvestmentYear { get; set; }
+            public decimal FutureSavingsGoal { get; set; }
+            public int WithdrawalAge { get; set; }
+            public decimal AnnualWithdrawalAmount { get; set; }
+        }
+
+    }
+}
